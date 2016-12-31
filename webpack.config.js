abf1934d4e08db654657f0a012dfe2cf67f5c7a1
JamesPF <jamesfelz@gmail.com>
2016-12-30 16:02:02 -0800

    Prepare config files for heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var webpack = require('webpack');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -12,6 +14,11 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
     })
   ],
   output: {
@@ -61,5 +68,6 @@ module.exports = {
         loader: 'url'
       }
     ]
-  }
+  },
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'inline-source-map'
 };
