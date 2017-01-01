b461151981cf6c1a0d03f63fb47bd66fc11dbcb2
JamesPF <jamesfelz@gmail.com>
2016-12-31 17:44:16 -0800

    Set up database for heroku and add JWT environment variables to user model

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/JournalApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/JournalApp');
 
 module.exports = {mongoose};
