4fe56c31aa6e3a7a0c205240faac940faab3a115
JamesPF <jamesfelz@gmail.com>
2016-12-31 18:32:36 -0800

    Add content to About page

--- app/components/about/AboutPage.jsx
+++ app/components/about/AboutPage.jsx
@@ -5,6 +5,20 @@ var AboutPage = React.createClass({
     return (
       <div id="about-page">
         <h1 className="text-center">About</h1>
+        <h3 className="text-center">Currently working on...</h3>
+        <p className="text-center">Here's what I'm currently working on in order to make this application better.</p>
+          <ul>
+            <li>Creating a better, less buggy method for updating entry titles and content</li>
+            <li>Displaying journals in alphabetical order</li>
+            <li>Displaying entries in terms of most recently updated</li>
+            <li>Adding the ability to delete entries</li>
+            <li>Adding the ability to delete journals</li>
+            <li>Testing for both front and back end</li>
+            <li>Overall styling of the whole application</li>
+          </ul>
+
+        <h3 className="text-center">This application is built with:</h3>
+        <p className="text-center">React JS, Node.js, Express, MongoDB, Bootstrap, and SCSS</p>
       </div>
     );
   }
