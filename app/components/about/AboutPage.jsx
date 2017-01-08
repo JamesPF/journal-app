d78875a6b51c8e07bd3e3f8010cb5136daf2a074
JamesPF <jamesfelz@gmail.com>
2017-01-08 11:32:12 -0800

    Finish responsive styling on entries page

--- app/components/about/AboutPage.jsx
+++ app/components/about/AboutPage.jsx
@@ -9,9 +9,9 @@ var AboutPage = React.createClass({
         <p className="text-center">Here's what I'm currently working on in order to make this application better.</p>
           <ul className="updates-list">
             <li>Displaying journals in alphabetical order</li>
-            <li>Displaying entries in terms of most recently updated</li>
-            <li>Adding the ability to delete entries</li>
-            <li>Adding the ability to delete journals</li>
+            <li>Displaying entries by most recently updated</li>
+            <li>Functionality for deleting entries</li>
+            <li>Functionality for deleting journals</li>
             <li>Testing for both front and back end</li>
             <li>Overall styling of the whole application</li>
           </ul>
